fd6bd8c42daff83be8a7383bd11db3dc2e086478
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2022-07-25 15:59:59 +0530

    some Changes

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,12 +2,27 @@
 
 const express = require("express");
 const router = express.Router();
-const { userRegister, loginUser } = require("../controllers/userApi");
+const {
+  userRegister,
+  loginUser,
+  getProfile,
+  UpdateProfile,
+} = require("../controllers/userApi");
+
+const { Authentication, Authorization } = require("../middlewares/auth");
 
 //------------------- API and Method Routes-------------------//
 
+//------------------- User APIs ------------------------------//
 router.post("/register", userRegister);
 router.post("/login", loginUser);
+router.get(" /user/:userId/profile", Authentication, getProfile);
+router.put(
+  "/user/:userId/profile",
+  Authentication,
+  Authorization,
+  UpdateProfile
+);
 
 //------------------- Exporting Modules -------------------//
 
