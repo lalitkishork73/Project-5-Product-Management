77442be15834b3a6ef50b04608350902a59ad182
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2022-07-27 13:33:39 +0530

    get by product id

--- src/controllers/productsApi.js
+++ src/controllers/productsApi.js
@@ -1,4 +1,6 @@
-const productModel=require('../models/productModel');
+const productModel = require("../models/productModel");
+
+//<<============================ Create  Product  ==============================>>//
 
 const createProducts = async function (req, res) {
   try {
@@ -7,6 +9,8 @@ const createProducts = async function (req, res) {
   }
 };
 
+//<<============================ Get Product By Query Filter ==============================>>//
+
 const getProducts = async function (req, res) {
   try {
   } catch (err) {
@@ -14,14 +18,37 @@ const getProducts = async function (req, res) {
   }
 };
 
+//<<============================Get Product By Id ==============================>>//
+
 const getProductbyId = async function (req, res) {
   try {
-    let Pid= req.params.
+    let Pid = req.params.productId;
 
+    if (!isValid(Pid) && !isValidObjectId(Pid)) {
+      return res.status(400).send({
+        status: false,
+        message: "Invalid Product ID please Provide Valid Credential",
+      });
+    }
+
+    const findProductDb = await productModel.findOne({
+      _id: Pid,
+      isDeleted: false,
+    });
+
+    if (!findProductDb) {
+      return res.status(404).send({ status: false, message: "Data Not Found" });
+    }
+
+    return res
+      .status(200)
+      .send({ status: true, message: "success", data: findProductDb });
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
 };
+
+//<<============================ Update Product By Id ==============================>>//
 
 const updateProductbyId = async function (req, res) {
   try {
@@ -30,13 +57,14 @@ const updateProductbyId = async function (req, res) {
   }
 };
 
+//<<============================ Delete Product By Id ==============================>>//
+
 const deleteProductbyId = async function (req, res) {
   try {
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
 };
-
 
 module.exports = {
   createProducts,
