2adaa08b10159457b1df72d93e73f51112460c8a
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2022-08-02 20:00:12 +0530

    createCartDone

--- src/controllers/cartApi.js
+++ src/controllers/cartApi.js
@@ -1,7 +1,163 @@
 const cartModel = require("../models/cartModel");
+const userModel = require("../models/userModels");
+const productModel = require("../models/productModel");
+
+const {
+  isValid,
+  isValidRequestBody,
+  isValidObjectId,
+} = require("../utils/util");
 
 const createCart = async function (req, res) {
   try {
+    const CartData = req.body;
+    const userId = req.params.userId;
+    let { cartId, productId, quantity } = CartData;
+    // let data = {};
+
+    if (!isValidRequestBody(CartData)) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Please Provide Cart information" });
+    }
+
+    if (!isValidObjectId(userId)) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Please Provide valid User Id" });
+    }
+    //-------------------------------------
+    const findUserId = await userModel.findOne({ _id: userId });
+
+    if (!findUserId) {
+      return res.status(400).send({ status: false, message: "User not found" });
+    }
+
+    if (!isValid(productId)) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Please Provide Product Id" });
+    }
+
+    if (!isValidObjectId(productId)) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Please Provide valid Product Id " });
+    }
+    //--------------------------------------
+    const checkProduct = await productModel.findOne({
+      _id: productId,
+      isDeleted: false,
+    });
+
+    if (!checkProduct) {
+      return res
+        .status(400)
+        .send({ status: false, message: "No Such Product Exist" });
+    }
+
+    // data.productId = productId;
+    if (!quantity) {
+      quantity = 1;
+    }
+
+    if (quantity) {
+      if (typeof quantity !== "number" || quantity <= 0) {
+        return res
+          .status(400)
+          .send({ status: false, message: "Enter Valid Quantity" });
+      }
+    }
+
+    if (!cartId) {
+      let cart = await cartModel.findOne({ userId: userId });
+      if (cart) {
+        return res.status(400).send({
+          status: false,
+          message: `${cart.userId} with this userId cart is already present ${cart._id} this is your cart id `,
+        });
+      }
+      if (!cart) {
+        const addToCart = {
+          userId: userId,
+          items: {
+            productId: productId,
+            quantity: quantity,
+          },
+          totalPrice: checkProduct.price * quantity,
+          totalItems: 1,
+        };
+
+        const newCart = await cartModel.create(addToCart);
+        return res.status(201).send({
+          status: true,
+          message: "cart created and product added to cart successfully",
+          data: newCart,
+        });
+      }
+    }
+
+    if (cartId) {
+      if (!isValidObjectId(cartId)) {
+        return res
+          .status(400)
+          .send({ status: false, message: "CartId is Not Valid" });
+      }
+
+      let checkCartId = await cartModel.findOne({ _id: cartId });
+
+      if (!checkCartId) {
+        return res.status(400).send({
+          status: false,
+          message: `cart not exist with this id ${cartId} so create cart first`,
+        });
+      }
+
+      if (checkCartId.userId.toString() != findUserId._id.toString()) {
+        return res
+          .status(400)
+          .send({ status: false, message: `userId not match to cart ` });
+      }
+
+      //for Same Product Id
+      let arr = checkCartId.items;
+      let priceSum = checkCartId.totalPrice + checkProduct.price * quantity;
+
+      if(checkCartId){
+      for (let i in arr) {
+        if (arr[i].productId.toString() == productId) {
+          arr[i].quantity++;
+          checkCartId.totalPrice = priceSum;
+          checkCartId.totalItems = arr.length;
+
+          checkCartId.save();
+           return res.status(200).send({
+             status: true,
+             message: `Quantity of ${arr[i].productId} increases`,
+             data: checkCartId,
+           });
+        }
+
+      }
+       checkCartId.items[checkCartId.items.length] = {
+         productId: productId,
+         quantity: quantity,
+       };
+       checkCartId.totalPrice =priceSum;
+       checkCartId.totalItems = checkCartId.items.length;
+      checkCartId.save();
+      return res
+        .status(200)
+        .send({
+          status: true,
+          message: `New product ${checkCartId._id} added in cart`,
+          data: checkCartId,
+        });
+
+
+    }
+      console.log(checkCartId);
+    }
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
--- src/models/cartModel.js
+++ src/models/cartModel.js
@@ -1,27 +1,46 @@
 const mongoose = require("mongoose");
-const ObjectId = mongoose.schema.Types.ObjectId;
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const cartShema = new mongoose.Schema({
-    userId : {type: ObjectId, ref: "user", required: true, unique: true},
-    items: [{
-        productId: {type: ObjectId, product, unique: true},
-        quantity: {type: Number, required: true, min: 1}
-    }],
-    totalPrice: {type: Number, required: true, },
-    totalItems: {type: Number, required: true, },
-}, { timestamps: true });
+const cartShema = new mongoose.Schema(
+  {
+  userId: {
+    type: ObjectId,
+    ref: 'user',
+    required: 'userId is required',
+    trim: true,
+    unique: true
+  },
+  items: [{
+    productId: {
+      type: ObjectId,
+      ref: 'product',
+      // required: true,
+      unique: true,
+      trim: true,
+
+    },
+    quantity: {
+      type: Number,
+      // required: true,
+      trim: true,
+      min: 1
+    },
+_id: false
+
+  }],
+  totalPrice: {
+    type: Number,
+    // required: true,
+    trim: true
+  },
+
+  totalItems: {
+    type: Number,
+    // required: true,
+    trim: true
+  }
+},
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("cart", cartShema);
-
-
-// {
-//     userId: {ObjectId, refs to User, mandatory, unique},
-//     items: [{
-//       productId: {ObjectId, refs to Product model, mandatory},
-//       quantity: {number, mandatory, min 1}
-//     }],
-//     totalPrice: {number, mandatory, comment: "Holds total price of all the items in the cart"},
-//     totalItems: {number, mandatory, comment: "Holds total number of items in the cart"},
-//     createdAt: {timestamp},
-//     updatedAt: {timestamp},
-//   }
