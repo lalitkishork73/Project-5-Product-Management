c6ed19f53a9725b2be3f7bb654286fdb611ec7bd
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2022-08-02 20:08:07 +0530

    CartAlmostDone

--- src/controllers/cartApi.js
+++ src/controllers/cartApi.js
@@ -8,6 +8,8 @@ const {
   isValidObjectId,
 } = require("../utils/util");
 
+//<<============================= Create Cart =================================>>//
+
 const createCart = async function (req, res) {
   try {
     const CartData = req.body;
@@ -123,101 +125,132 @@ const createCart = async function (req, res) {
       let arr = checkCartId.items;
       let priceSum = checkCartId.totalPrice + checkProduct.price * quantity;
 
-      if(checkCartId){
-      for (let i in arr) {
-        if (arr[i].productId.toString() == productId) {
-          arr[i].quantity++;
-          checkCartId.totalPrice = priceSum;
-          checkCartId.totalItems = arr.length;
+      if (checkCartId) {
+        for (let i in arr) {
+          if (arr[i].productId.toString() == productId) {
+            arr[i].quantity++;
+            checkCartId.totalPrice = priceSum;
+            checkCartId.totalItems = arr.length;
 
-          checkCartId.save();
-           return res.status(200).send({
-             status: true,
-             message: `Quantity of ${arr[i].productId} increases`,
-             data: checkCartId,
-           });
+            checkCartId.save();
+            return res.status(200).send({
+              status: true,
+              message: `Quantity of ${arr[i].productId} increases`,
+              data: checkCartId,
+            });
+          }
         }
-
-      }
-       checkCartId.items[checkCartId.items.length] = {
-         productId: productId,
-         quantity: quantity,
-       };
-       checkCartId.totalPrice =priceSum;
-       checkCartId.totalItems = checkCartId.items.length;
-      checkCartId.save();
-      return res
-        .status(200)
-        .send({
+        checkCartId.items[checkCartId.items.length] = {
+          productId: productId,
+          quantity: quantity,
+        };
+        checkCartId.totalPrice = priceSum;
+        checkCartId.totalItems = checkCartId.items.length;
+        checkCartId.save();
+        return res.status(200).send({
           status: true,
           message: `New product ${checkCartId._id} added in cart`,
           data: checkCartId,
         });
-
-
-    }
+      }
       console.log(checkCartId);
     }
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
 };
+
+//<<============================= Update Cart =================================>>//
+
 const updateCart = async function (req, res) {
   try {
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
 };
+
+//<<============================= Get Cart Details =================================>>//
+
 const getCart = async function (req, res) {
   try {
     let userId = req.params.userId;
-   
+
     //----------------------------------------------Validation Starts---------------------------------------//
     // validating userid from params
     if (!isValid(userId)) {
-        return res.status(400).send({ status: false, message: "Invalid request parameters. userId is required" });
+      return res.status(400).send({
+        status: false,
+        message: "Invalid request parameters. userId is required",
+      });
     }
     if (!isValidObjectId(userId)) {
-        return res.status(400).send({ status: false, message: "Invalid request parameters. userId is not valid" });
+      return res.status(400).send({
+        status: false,
+        message: "Invalid request parameters. userId is not valid",
+      });
     }
-    let user = await userModel.findOne({ _id: userId })
+    let user = await userModel.findOne({ _id: userId });
     if (!user) {
-        return res.status(404).send({ status: false, msg: "No such user found. Please register and try again" });
+      return res.status(404).send({
+        status: false,
+        msg: "No such user found. Please register and try again",
+      });
     }
     let usercartid = await cartModel.findOne({ userId: userId });
     if (!usercartid) {
-        return res.status(404).send({ status: false, msg: "No such cart found. Please register and try again" });
+      return res.status(404).send({
+        status: false,
+        msg: "No such cart found. Please register and try again",
+      });
     }
-   
-    return res.status(200).send({ status: true, data: usercartid })
+
+    return res.status(200).send({ status: true, data: usercartid });
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
 };
+
+//<<============================= Delete Cart  =================================>>//
+
 const deleteCart = async function (req, res) {
   try {
     let userId = req.params.userId;
-        
-        //--------------------------- ---------------------Validation Starts-------------------------------------//
-        // validating userid from params
-        if (!isValid(userId)) {
-            return res.status(400).send({ status: false, message: "Invalid request parameters. userId is required" });
-        }
-        if (!isValidObjectId(userId)) {
-            return res.status(400).send({ status: false, message: "Invalid request parameters. userId is not valid" });
-        }
 
-        let Userdata = await userModel.findOne({ _id: userId })
-        if (!Userdata) {
-            return res.status(404).send({ status: false, msg: "No such user exists with this userID" });
-        }
-      
-        let usercart = await cartModel.findOne({ userId: userId })
-        if (!usercart) {
-            return res.status(404).send({ status: false, msg: "No such user found. Please register and try again" });
-        }
-        let updatedUserCart = await cartModel.findOneAndUpdate({ userId: userId }, { items: [], totalPrice: 0, totalItems: 0 }, { new: true })
-        return res.status(200).send({ status: true })
+    //--------------------------- ---------------------Validation Starts-------------------------------------//
+    // validating userid from params
+    if (!isValid(userId)) {
+      return res.status(400).send({
+        status: false,
+        message: "Invalid request parameters. userId is required",
+      });
+    }
+    if (!isValidObjectId(userId)) {
+      return res.status(400).send({
+        status: false,
+        message: "Invalid request parameters. userId is not valid",
+      });
+    }
+
+    let Userdata = await userModel.findOne({ _id: userId });
+    if (!Userdata) {
+      return res
+        .status(404)
+        .send({ status: false, msg: "No such user exists with this userID" });
+    }
+
+    let usercart = await cartModel.findOne({ userId: userId });
+    if (!usercart) {
+      return res.status(404).send({
+        status: false,
+        msg: "No such user found. Please register and try again",
+      });
+    }
+    let updatedUserCart = await cartModel.findOneAndUpdate(
+      { userId: userId },
+      { items: [], totalPrice: 0, totalItems: 0 },
+      { new: true }
+    );
+    return res.status(200).send({ status: true });
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
