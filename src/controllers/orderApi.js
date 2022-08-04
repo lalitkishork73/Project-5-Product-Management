02abf98fc89776aa5604ca696c7c93b4e98aafc6
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2022-08-04 15:01:40 +0530

    orderModel added

--- src/controllers/orderApi.js
+++ src/controllers/orderApi.js
@@ -8,8 +8,11 @@ const createOrder = async function (req, res) {
   }
 };
 
+
 const updateOrder = async function (req, res) {
   try {
+  
+
   } catch (err) {
     return res.status(500).send({ status: false, message: err.message });
   }
--- src/controllers/userApi.js
+++ src/controllers/userApi.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const userModel = require("../models/userModels");
+const userModel = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const { uploadFile } = require("./awsConnect.js");
 
