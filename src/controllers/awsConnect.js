d11f7fc502eca7f2b30e01ce089a8e5dc436760f
Lalitkishor Kanojiya <106534721+lalitkishork73@users.noreply.github.com>
2023-01-04 18:52:52 +0530

    added

--- src/controllers/awsConnect.js
+++ src/controllers/awsConnect.js
@@ -3,6 +3,7 @@ const aws = require("aws-sdk");
 aws.config.update({
   accessKeyId: "AKIAR7VN4HY7UGJZDEE2",
   secretAccessKey: "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",
+  region:"ap-south-1"
 });
 
 let uploadFile = async (file) => {
@@ -10,6 +11,7 @@ let uploadFile = async (file) => {
     let s3 = new aws.S3({ apiVersion: "2006-03-01" });
 
     var uploadParams = {
+      ACL: "public-read",
       Bucket: "bucketwala",
       Key: "me/" + file.originalname,
       Body: file.buffer,
